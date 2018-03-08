bb1f817fb5809999a7f75b8878385ba426bfb9cc
quan <quanluis@protonmail.com>
2018-03-08 18:56:02 -0500

    created reducers and combined them

--- src/store.js
+++ src/store.js
@@ -1,7 +1,16 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+import productReducer from './products/';
+import cartrReducer from './cart/';
+import searchReducer from './search/'
+
+const rootReducer = combineReducers({
+    products: productReducer,
+    search: searchReducer,
+    cart: cartReducer
+});W
 
  const store = createStore(
-     () => {},
+     rootReducer,
      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
  )
  export default store;
